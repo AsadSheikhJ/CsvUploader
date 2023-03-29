422a651b9839a4dd4b4592e70891950a3b9633be
AsadSheikhJ <sheikhasad1652@gmail.com>
2023-03-29 12:24:19 +0500

    changes

--- upload_excel.php
+++ upload_excel.php
@@ -1,50 +1,5 @@
 <?php
 
-// define the hard-coded header values for each file name
-// $header_values = array(
-  // // "clinical_stool.csv" => array(
-  //   "S.NO",
-  //   "Name",
-  //   "Gender M/F",
-  //   "Specimen ID",
-  //   "Age",
-  //   "Enrollment ID",
-  //   "Date of Collection",
-  //   "Town",
-  //   "Union Council 1)UC-4 GUJRO 2)UC-8 MANGOPIR 3)UC-5 SONGAL",
-  //   "Stool Sample Collected In Wide Mouth Container Yes/No",
-  //   "Stool Cary-Blair Received Yes/No",
-  //   "FIELD SITE",
-  //   "Time of Collection (hh:mm, 24 hr clock)",
-  //   "Date Received (dd:mm:yy)",
-  //   "Stool Collected By",
-  //   "BS#",
-  //   "Organism Isolated",
-  //   "(S=≥17,I=14-16,R=≤13)",
-  //   "(S=≥18,I=13-17,R=≤12)",
-  //   "(S=≥13,R=≤13)",
-  //   "(S=≥16,I=11-15,R=?)",
-  //   "(S=≥19,I=15-18,R=≤14)",
-  //   "(S=≥21,I=16-20,R=≤15)",
-  //   "(S=≥17,I=14-16,R=≤13)",
-  //   "(S=≥18,I=13-17,R=≤12)",
-  //   "(S=≥16,I=11-15,R=≤10)",
-  //   "(S=≥23,I=20-22,R=≤19)",
-  //   "(S=≥31,I=21-30,R=≤20)",
-  //   "(S=≥13,I=N/A,R=≤12)",
-  //   "(S=≥19,I=16-18,R=≤15)",
-  //   "(S=≥23,I=20-22,R=≤19)",
-  //   "(S=≥23,I=20-22,R=≤19)",
-  //   "(S=≥17,I=14-16,R=≤13)",
-  //   "(S=≥19,I=14-18.R=≤13)",
-  //   "(S=≥16,I=11-15,R=≤10)",
-  //   "(S=≥26,I=23-25,R=≤22)",
-  //   "(S=≥26,I=22-25,R=≤21)",
-  //   "(≥16,11-15,≤10)"
-  // ),
-//   "file2.csv" => array("HeaderA", "HeaderB", "HeaderC", "HeaderD")
-// );
-
 // Replace the values below with your own database connection details
 $host = 'localhost';
 $username = 'root';
@@ -111,6 +66,25 @@ $headerMap = [
         "Negative",
         "New Reported COVID-19 Cases (within 24 hours)",
         "Samples Taken"
+    ],
+    'covid_gadap' => [
+        "Date (DD/MM/YYYY)",
+        "Negative",
+        "New Reported COVID-19 Cases (within 24 hours)",
+        "Samples Taken"
+    ],
+    'rota_case' => [
+        "PARVAAN STUDY ID",
+        "Child's Sex:",
+        "Did you collected the stool sample:",
+        "Date of stool Collection (DD/MM/YYYY)",
+        "ELISA Result"
+    ],
+    'enteric_fever' => [
+        "Town",
+        "Date of blood collection",
+        "Name of Lab",
+        "Species"
     ]
     // add more file headers as needed
 ];
@@ -121,6 +95,9 @@ $iteratorRow = [
     'cholera_case' => 4,
     'covid_layari' => 4,
     'covid_gulshan' => 4,
+    'covid_gadap' => 4,
+    'rota_case' => 4,
+    'enteric_fever' => 4
     // add more file headers as needed
 ];
 
@@ -136,14 +113,12 @@ if (!isset($headerMap[$selectedFile])) {
 }
 
 // Load the PHPExcel library (make sure it's installed)
-// require_once 'path/to/PHPExcel.php';
 require 'PhpSpreadsheet/vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 // Load the Excel file
-// $objPHPExcel = PHPExcel_IOFactory::load($excelFile);
 $objPHPExcel = IOFactory::load($excelFile);
 
 // Get the first worksheet
@@ -157,7 +132,9 @@ foreach ($headers as $header) {
 }
 
 echo "<pre>";
+echo "Hardcoded";
 var_dump($headerMap[$selectedFile]);
+echo "Provided";
 var_dump($headerRow);
 echo "</pre>";
 
@@ -173,14 +150,15 @@ $statement = $pdo->prepare("SELECT COUNT(*) FROM information_schema.columns WHER
 $statement->execute([$tableName]);
 $columnCount = $statement->fetchColumn();
 
+// Get the iterator row for the selected file
+$iteratorRow = isset($iteratorRow[$selectedFile]) ? $iteratorRow[$selectedFile] : 1;
+
 // Get the data rows from the worksheet
 $dataRows = $worksheet->toArray(null, false, false, true);
 
-array_shift($dataRows); // remove the header row
-array_shift($dataRows); // remove the header row
-array_shift($dataRows); // remove the header row
+// Remove the header rows based on the iterator row
+array_splice($dataRows, 0, $iteratorRow - 1);
 
-    // var_dump($dataRows)  ;
 // Check if the number of columns in the data rows matches the table's column count
 foreach ($dataRows as $row) {
     if (count($row) != $columnCount) {
